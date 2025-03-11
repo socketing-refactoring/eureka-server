8394d3ce49070912e0e507b74203f55bdea16914
jee-in <chris309804@gmail.com>
2025-03-11 19:55:50 +0900

    chore: Remove Spring secuirty dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ val springCloudVersion = "2024.0.0"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+//	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-actuator:3.4.3")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
 	implementation(platform("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion"))
@@ -29,7 +29,7 @@ dependencies {
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 
-	testImplementation("org.springframework.security:spring-security-test")
+//	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testCompileOnly("org.projectlombok:lombok")
--- src/main/java/com/jeein/eureka/SecurityConfig.java
+++ src/main/java/com/jeein/eureka/SecurityConfig.java
@@ -1,46 +1,46 @@
-package com.jeein.eureka;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity.csrf(auth -> auth.disable());
-        httpSecurity.authorizeHttpRequests(auth->auth.anyRequest().authenticated());
-        httpSecurity.httpBasic(Customizer.withDefaults());
-        return httpSecurity.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(@Value("${user.username}") String username,
-                                                 @Value("${user.password}") String password,
-                                                 @Value("${user.role}") String role) {
-        UserDetails user = User.builder()
-                .username(username)
-                .password(bCryptPasswordEncoder().encode(password))
-                .roles(role)
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }
-}
-
+//package com.jeein.eureka;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+//        httpSecurity.csrf(auth -> auth.disable());
+//        httpSecurity.authorizeHttpRequests(auth->auth.anyRequest().authenticated());
+//        httpSecurity.httpBasic(Customizer.withDefaults());
+//        return httpSecurity.build();
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService(@Value("${user.username}") String username,
+//                                                 @Value("${user.password}") String password,
+//                                                 @Value("${user.role}") String role) {
+//        UserDetails user = User.builder()
+//                .username(username)
+//                .password(bCryptPasswordEncoder().encode(password))
+//                .roles(role)
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
+//}
+//
